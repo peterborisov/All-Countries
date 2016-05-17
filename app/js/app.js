49403a5d4afe64f223c5654fa751651b2a5a53f1
peterborisov <borisov.peter@yahoo.co.uk>
2016-05-17 10:50:37 +0300

    clear files

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('issueTracking', ['ngRoute', 'ngResource', 'ngStorage'])
+var app = angular.module('allCountries', ['ngRoute', 'ngResource', 'ngStorage'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/'});
-    }])
-    .constant('BASE_URL', 'http://softuni-issue-tracker.azurewebsites.net/api/')
-    .constant('pageSize', '2');
+    }]);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
--- app/js/controllers/homeController.js
+++
@@ -1,4 +0,0 @@
-app.controller('HomeController', ['$scope', 'authenticationService',
-    function ($scope, authenticationService) {
-        $scope.isLogged = authenticationService.isLoggedIn;
-    }]);
